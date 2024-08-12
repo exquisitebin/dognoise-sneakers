13556307e0040853337e086f143542a7821424a2
Tristan Clark <thclark2000@gmail.com>
2024-08-12 20:48:31 +1000

    initialise and print

--- dognoise.py
+++ dognoise.py
@@ -1,31 +1,32 @@
 from gpiozero import LED, Button
-from signal import pause
-from pygame import mixer
+# from signal import pause
+# from pygame import mixer
 from os import walk, path
 
 
-def load_sounds():
-    mixer.init()
-    sounds = {}
-    exts = ('.wav', '.mp3')
-    for root, _, files in walk('sounds'):
-        for filename in files:
-            if filename.endswith(exts):
-                sounds[filename.split('.')[0]] = mixer.Sound(path.join(root, filename))
-    return sounds
+# def load_sounds():
+#     mixer.init()
+#     sounds = {}
+#     exts = ('.wav', '.mp3')
+#     for root, _, files in walk('sounds'):
+#         for filename in files:
+#             if filename.endswith(exts):
+#                 sounds[filename.split('.')[0]] = mixer.Sound(path.join(root, filename))
+#     return sounds
     
 
 
 def dognoise():
     # sounds = load_sounds()
-
+    print("Dog noise started")
+    
     bottom_btn = Button(2)
 
     while True:
         if bottom_btn.is_pressed:
             bark()
         else:
-            pass
+            print("No button press")
 
 
 

1c927d7db45e9078e7ab34042ebd69cf90c4b646
Tristan Clark <thclark2000@gmail.com>
2024-08-12 21:38:20 +1000

    Add sound

--- dognoise.py
+++ dognoise.py
@@ -1,38 +1,40 @@
 from gpiozero import LED, Button
-# from signal import pause
-# from pygame import mixer
+from signal import pause
+import pygame
 from os import walk, path
 
 
-# def load_sounds():
-#     mixer.init()
-#     sounds = {}
-#     exts = ('.wav', '.mp3')
-#     for root, _, files in walk('sounds'):
-#         for filename in files:
-#             if filename.endswith(exts):
-#                 sounds[filename.split('.')[0]] = mixer.Sound(path.join(root, filename))
-#     return sounds
+def load_sounds():
+    pygame.init()
+    sounds = {}
+    exts = ('.wav', '.mp3')
+    for root, _, files in walk('sounds'):
+        for filename in files:
+            if filename.endswith(exts):
+                sounds[filename.split('.')[0]] = pygame.mixer.Sound(path.join(root, filename))
+    return sounds
     
 
-def bark():
+def bark(sounds):
     print("Woof!")
+    sounds['bark'].play()
+    
 
 def dognoise():
-    # sounds = load_sounds()
+    sounds = load_sounds()
     print("Dog noise started")
 
     bottom_btn = Button(27)
 
+    pressed = False
+
     while True:
         if bottom_btn.is_pressed:
-            print("Woof!")
-        # else:
-            # print("No button press")
-
-
-
-    
+            if not pressed:
+                pressed = True
+                bark()
+        else:
+            pressed = False
 
 
 if __name__ == '__main__':

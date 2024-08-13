379184af32592ebc4774e04881f39b6aef063194
Tristan Clark <thclark2000@gmail.com>
2024-08-13 20:53:29 +1000

    distance sensor

--- dognoise.py
+++ dognoise.py
@@ -1,7 +1,15 @@
-from gpiozero import LED, Button
-from signal import pause
+from gpiozero import Button, DistanceSensor
+from time import sleep
+from signal import signal, SIGTERM, SIGHUP, pause
 import pygame
 from os import walk, path
+
+def safe_exit(signum, frame):
+    print("Exiting")
+    pygame.quit()
+    exit(0)
+
+
 
 
 def load_sounds():
@@ -24,21 +32,37 @@ def bark(sounds):
     
 
 def dognoise():
+    sensor = DistanceSensor(echo=20, trigger=21)
     sounds = load_sounds()
     print("Dog noise started")
+
+
+
 
     bottom_btn = Button(27)
 
     pressed = False
+    try:
+        while True:
+            if bottom_btn.is_pressed:
+                if not pressed:
+                    pressed = True
+                    bark(sounds)
+            else:
+                pressed = False
 
-    while True:
-        if bottom_btn.is_pressed:
-            if not pressed:
-                pressed = True
-                bark(sounds)
-        else:
-            pressed = False
+            print(sensor.value)
+            sleep(0.1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        sensor.close()
+        print("Cleaning up")
+        pygame.quit()
+        exit(0)
 
 
 if __name__ == '__main__':
+    signal(SIGTERM, safe_exit)
+    signal(SIGHUP, safe_exit)
     dognoise()
